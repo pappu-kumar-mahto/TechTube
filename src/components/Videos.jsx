27aaa1b4de66d5c10941ae349c092582633f4dda
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-08-29 12:39:09 +0530

    Project TechTube: ChannelTitle Card completed.

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,11 +4,11 @@ import {VideoCard, ChannelCard} from './'
 
 const Videos = ({ videos }) => {
   return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={1}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item}/>}
-          {/* {item.id.channelId && <ChannelCard channelDetail={item}/>} */}
+          {item.id.channelId && <ChannelCard channelDetail={item}/>}
         </Box>
       ))}
       
