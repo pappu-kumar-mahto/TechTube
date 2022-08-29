0d2ae8b07c09b88d184aa5e699c6c8f1e6dfbd33
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-08-29 14:07:06 +0530

    Project TechTube: Channel Detail part completed.

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>
-      ChannelDetail
-    </div>
-  )
-}
+  const { id } = useParams();
 
-export default ChannelDetail
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order='date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh" color="white">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(248,147,29,1) 23%, rgba(0,0,254,1) 100%)",
+            zIndex: 10,
+            height: "250px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box dislay="flex" p={2}>
+        <Box
+          sx={{
+            mr: { sm: "100px" },
+          }}
+        />
+          <Videos videos={videos}/>
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,12 +5,12 @@ import {VideoCard, ChannelCard} from './'
 const Videos = ({ videos }) => {
   return (
     <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={1}>
-      {videos.map((item, idx) => (
+      {videos ? videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item}/>}
           {item.id.channelId && <ChannelCard channelDetail={item}/>}
         </Box>
-      ))}
+      )): 'Loading'}
       
     </Stack>
   )
