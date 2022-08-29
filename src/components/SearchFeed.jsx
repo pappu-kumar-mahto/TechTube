9655205d5ca694fb79827728768db2167d4c1b77
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-08-29 14:24:52 +0530

    Project TechTube: search part completed.

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import {Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
 
 const SearchFeed = () => {
-  return (
-    <div>
-      SearchFeed
-    </div>
-  )
-}
+  const {searchTerm} = useParams()
 
-export default SearchFeed
+  const [videos, setVideos] = useState([])
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => setVideos(data.items) )
+  },[searchTerm])
+
+  return (
+    <Box
+        p={2}
+        sx={{
+          overflowY: 'auto',
+          height: '90vh',
+          flex: 2,
+          color:'#fff'
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight='bold'
+          mb={2}
+          sx={{
+            color:'white'
+          }}
+        >Search Results for:
+          <span style={{
+            color: '#F7931D'
+        }}>{` ${searchTerm} `}</span>videos
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+  );
+};
+
+export default SearchFeed;
